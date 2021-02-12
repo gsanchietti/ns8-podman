d120f09fcd30a3c68de1254af3ba6150b8875e99
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2021-02-12 16:12:20 +0000

    Add redis

---
+++ redis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# DOC: https://hub.docker.com/_/redis/
+
+N=ns-redis
+DIR=/var/lib/nethserver/redis
+
+# configure persistente dir
+mkdir -p $DIR/data
+
+podman pod stop $N-pod
+podman pod rm $N-pod
+        
+podman pod create --name $N-pod -p 6379:6379
+
+podman run -d --volume $DIR/data:/data:Z \
+	--pod $N-pod --name $N docker.io/redis:6-alpine  --appendonly yes
