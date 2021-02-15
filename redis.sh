90ab27d374fcef6beed67c4c2176ce5352a57362
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2021-02-15 09:39:27 +0000

    redis: expose port only on localhost

--- redis.sh
+++ redis.sh
@@ -11,7 +11,7 @@ mkdir -p $DIR/data
 podman pod stop $N-pod
 podman pod rm $N-pod
         
-podman pod create --name $N-pod -p 6379:6379
+podman pod create --name $N-pod -p 127.0.0.1:6379:6379
 
 podman run -d --volume $DIR/data:/data:Z \
 	--pod $N-pod --name $N docker.io/redis:6-alpine  --appendonly yes
