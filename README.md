0570308b22fc0e201cc161ff06ff4b250fcc2762
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2021-02-15 10:39:49 +0000

    Add wikijs

--- README.md
+++ README.md
@@ -28,7 +28,9 @@ First, start redis by executing: `./redis.sh`
 
 ### Dokuwiki
 
-Put the configuration on REDIS:
+Configure a [Dokuwiki]/https://www.dokuwiki.org/) instance.
+
+Put the configuration on Redis:
 ```
 redis-cli hmset service/dokuwiki/env DOKUWIKI_USER admin
 redis-cli SADD service/dokuwiki/paths /var/lib/nethserver/dokuwiki:1001:1001
@@ -40,4 +42,20 @@ Then start the pod: `./dokuwiki.sh`
 
 The script will start a dokuwiki instance with valid SSL certificate, persistence and redirection from HTTP to HTTPs
 Default host for the dokuwiki is ``dokuwiki.<fqdn>``, make sure to have a valid DNS public record for it.
-  
+
+## Wiki.js
+
+Configure a [Wiki.js](https://js.wiki/) instance with a Postgres database.
+
+Put the configuration on Redis:
+```
+redis-cli hmset service/wikijs/env POSTGRES_DB wiki POSTGRES_PASSWORD wikipass POSTGRES_USER wikijs DB_TYPE postgres DB_HOST ns-wikijs-db DB_PORT 5432 DB_USER wikijs DB_PASS wikipass DB_NAME wiki
+redis-cli SADD service/wikijs/volumes db-data:/var/lib/postgresql/data
+redis-cli SADD service/wikijs/paths /var/lib/nethserver/wikijs:root:root
+redis-cli SET service/wikijs/hostname wikijs.nethserver.org
+```
+
+Then start the pod: `./wikijs.sh`
+
+The script will start a dokuwiki instance with valid SSL certificate, persistence and redirection from HTTP to HTTPs
+Default host for the wikijs is ``wikijs.<fqdn>``, make sure to have a valid DNS public record for it.
---
+++ wikijs.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# DOC: https://docs.requarks.io/install/docker
+
+N=ns-wikijs
+
+# configure environment variable
+# example: redis-cli hmset service/wikijs/env DOKUWIKI_USER admin
+i=0
+envs=''
+while read -r line
+do
+    if [[ -z "$line" ]]; then
+        continue
+    fi
+    if [[ $i%2 -gt 0 ]]; then
+        envs="$envs=$line"
+    else
+        envs="$envs -e $line"
+    fi
+    i=$((i+1))
+done < <(redis-cli --raw hgetall service/wikijs/env)
+
+# configure persistente dir
+# expected format: /path/:uid:gid
+# example: redis-cli SADD service/wikijs/paths /var/lib/nethserver/wikijs:1001:1001
+paths=''
+while read -r line
+do
+    path=$(echo $line | cut -d':' -f1)
+    uid_gid=$(echo $line | cut -d':' -f2,3)
+
+    mkdir -p $path
+    chown $uid_gid $path
+done < <(redis-cli --raw SMEMBERS service/wikijs/paths)
+
+# map volumes
+# expected format: host_path:container:path
+# example: redis-cli SADD service/wikijs/volumes /var/lib/nethserver/wikijs:/bitnami/wikijs
+vols=''
+while read -r line
+do
+    vols="$vols --volume $line:Z "
+done < <(redis-cli --raw  SMEMBERS service/wikijs/volumes)
+
+# set virtual host name
+# example: redis-cli SET service/wikijs/hostname mywiki.nethserver.org
+HOST=$(redis-cli --raw GET service/wikijs/hostname)
+if [ -z "$HOST" ]; then
+    HOST=wikijs.$(hostname -f)
+fi
+
+podman pod stop $N-pod
+podman pod rm $N-pod
+        
+podman pod create --name $N-pod
+
+podman run -d $envs \
+    $vols \
+    --pod $N-pod --name $N-db docker.io/postgres:11-alpine
+
+
+podman run -d $envs \
+	-l "traefik.http.services.$N.loadbalancer.server.port=3000" \
+	-l "traefik.http.routers.$N-http.entrypoints=http,https" \
+	-l "traefik.http.routers.$N-http.rule=Host(\`$HOST\`)" \
+	-l "traefik.http.middlewares.http-to-https.redirectscheme.scheme=https" \
+	-l "traefik.http.routers.$N-http.middlewares=http-to-https@docker" \
+	-l "traefik.http.routers.$N-https.entrypoints=http,https" \
+	-l "traefik.http.routers.$N-https.rule=Host(\`$HOST\`)" \
+	-l "traefik.http.routers.$N-https.tls=true" \
+	-l "traefik.http.routers.$N-https.tls.certresolver=letsencrypt" \
+	-l "traefik.http.routers.$N-https.tls.domains[0].main=$HOST" \
+	--pod $N-pod --name $N docker.io/requarks/wiki:2
